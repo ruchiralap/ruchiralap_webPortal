d45e68f752919af9785a3074c7cc5205578e8fb9
SM RIYAD <smriyad0185@gmail.com>
2024-05-29 19:20:28 +0600

    added navbar

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { AlignLeft, ChevronRight, ShoppingCart, X } from "lucide-react";
+import { AlignLeft, ChevronRight, ShoppingCart, X ,Phone} from "lucide-react";
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import IsSearch from "./IsSearch";
@@ -7,9 +7,10 @@ function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-[#FFFBE8] text-black font-roboto">
+    <nav className="bg-[#ece7d4a8] bg-gradient-to-r from-[#ece7d4a8] via-[#fff0b3]
+     to-[#ece7d4a8] text-black font-roboto">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
+        <div className="relative flex items-center justify-between text-xl  h-40">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button */}
             <button
@@ -23,7 +24,7 @@ function Header() {
               {isOpen ? <X /> : <AlignLeft />}
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-start">
+          <div className="flex-1 flex items-center justify-center">
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-5">
                 <NavLink
@@ -78,20 +79,39 @@ function Header() {
             </div>
           </div>
 
-          <div className="flex-shrink-0 flex md:items-center md:justify-center absolute md:left-1/2 transform md:-translate-x-1/2 ml-11">
+          <div className="flex-shrink-0 flex md:items-center md:justify-start absolute  transform md:-translate-x-1/2 ml-11">
             <Link to="/">
-              <h3 className=" md:text-4xl font-mono bg-flag mr-20">
-                Ruchir Alap
-              </h3>
+              {/* <h3 className=" md:text-4xl font-mono bg-flag mr-20">
+                Ruchir Alapp
+              </h3> */}
+
+
+             <img className="w-1/2" src="https://i.ibb.co/Xjnjnhh/rucir-alaplogo-removebg-preview.png" alt="" srcset="" />
+
             </Link>
           </div>
 
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="absolute inset-y-0 right-0 flex  items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <IsSearch />
 
+          
+
+            <div className="flex mx-5 text-xl my-3 ">
+              <div className="flex items-center pe-2">
+          
+              <Phone  />
+              </div>
+           
+            <div>
+            <p className=" text-sm">Call Us :</p>
+            <p  ><b>783264873 </b></p>
+            </div>
+           
+
+            </div>
             <button className="md:ml-4 p-1 rounded-full text-gray-400 hover:text-white">
               <span className="sr-only">View Cart</span>
-              <ShoppingCart className="text-[#201700]" />
+              <ShoppingCart className="text-[#201700] " />
             </button>
           </div>
         </div>
