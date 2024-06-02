b573a13274c191f02b1890739a015d8cd013ed59
MacBook Pro <naym100m@gmail.com>
2024-06-02 23:36:44 +0600

    update and added search filtering system

--- src/Shared/Header/IsSearch.jsx
+++ src/Shared/Header/IsSearch.jsx
@@ -1,6 +1,7 @@
 import { Search } from "lucide-react";
 import { useContext } from "react";
 import { FilterContext } from "../../Context/FilterProvider";
+import { Link } from "react-router-dom";
 
 const IsSearch = () => {
   const { handleInputChange } = useContext(FilterContext);
@@ -16,7 +17,9 @@ const IsSearch = () => {
           className=" bg-transparent border h-10 px-5 pr-10 rounded-full text-sm focus:outline-none md:w-72 w-[160px] md:border-[#201700]"
         />
         <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
-          <Search className="h-4 w-5 text-slate-900" />
+          <Link to="/allProducts">
+            <Search className="h-4 w-5 text-slate-900" />
+          </Link>
         </button>
       </div>
     </>
