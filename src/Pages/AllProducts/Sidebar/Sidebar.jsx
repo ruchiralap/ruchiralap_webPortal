92ef0197ab598306b10db8b41b304f38ea428d81
MacBook Pro <naym100m@gmail.com>
2024-06-04 17:05:06 +0600

    update and a responsive drwaer add close btn in drawer

--- src/Pages/AllProducts/Sidebar/Sidebar.jsx
+++ src/Pages/AllProducts/Sidebar/Sidebar.jsx
@@ -1,20 +1,32 @@
 /* eslint-disable react/prop-types */
-import { IoReorderThree } from "react-icons/io5";
+import { useContext, useRef } from "react";
+import { FilterContext } from "../../../Context/FilterProvider";
+import { GoChevronDown } from "react-icons/go";
+import { RxCross2 } from "react-icons/rx";
 import Categories from "./Categories/Categories";
 import Price from "./Price/Price";
 import Size from "./Size/Size";
 import Products from "../Products/Products";
-import { useContext } from "react";
-import { FilterContext } from "../../../Context/FilterProvider";
-import { GoChevronDown } from "react-icons/go";
 
 const Sidebar = () => {
   const { result, handleChange } = useContext(FilterContext);
+  const drawerCheckboxRef = useRef(null);
+
+  const handleCloseDrawer = () => {
+    if (drawerCheckboxRef.current) {
+      drawerCheckboxRef.current.checked = false;
+    }
+  };
 
   return (
     <>
       <div className="drawer lg:drawer-open">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <input
+          id="my-drawer-2"
+          type="checkbox"
+          className="drawer-toggle"
+          ref={drawerCheckboxRef}
+        />
         <div className="drawer-content p-5">
           <label htmlFor="my-drawer-2" className="lg:hidden">
             <div className="flex items-center gap-1 hover:border px-4 py-2 rounded-md bg-[#F6CF0F] text-[#201700] font-medium hover:bg-none absolute">
@@ -31,7 +43,14 @@ const Sidebar = () => {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
+
           <section className="menu p-10 w-80 min-h-full bg-base-200 md:bg-inherit text-base-content">
+            <div
+              className="flex items-center justify-end rounded-md px-4 py-2 border absolute right-5 hover:cursor-pointer shadow-md"
+              onClick={handleCloseDrawer}
+            >
+              <RxCross2 className=" text-xl" />
+            </div>
             <Categories handleChange={handleChange} />
             <Price handleChange={handleChange} />
             <Size handleChange={handleChange} />
