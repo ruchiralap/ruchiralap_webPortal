427c75058e292b64ffd0ab54fd027c2d87d543b6
MacBook Pro <naym100m@gmail.com>
2024-06-09 17:14:16 +0600

    added a new content

--- src/Pages/Offer/Offer.jsx
+++ src/Pages/Offer/Offer.jsx
@@ -1,4 +1,6 @@
 import SectionTitle from "../../Hooks/SectionTitle";
+import { offer } from "../../data/data";
+import OfferCad from "./OfferCad";
 
 const Offer = () => {
   return (
@@ -8,6 +10,11 @@ const Offer = () => {
           title="What We Offer For You"
           details="We Connect buyers and Sellers of Naturals Organic Environmentally sound products. We find the best and makers natural and organic."
         />
+        <div className=" mt-10 grid grid-cols-1 md:grid-cols-3 gap-10">
+          {offer.map((itm) => (
+            <OfferCad key={itm?.id} itm={itm} />
+          ))}
+        </div>
       </section>
     </>
   );
