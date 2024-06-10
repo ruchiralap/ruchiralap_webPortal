e667d1cd84f7d97aa29da48bcd848fc3c4e12ccc
MacBook Pro <naym100m@gmail.com>
2024-06-10 08:19:57 +0600

    updated a home page tabs section

--- src/Pages/Home/TrendingProducts/TrendingProducts.jsx
+++ src/Pages/Home/TrendingProducts/TrendingProducts.jsx
@@ -4,6 +4,12 @@ import "react-tabs/style/react-tabs.css";
 import useCategory from "../../../Hooks/useCategory";
 import useAllProducts from "../../../Hooks/useAllProducts";
 import TrendingCard from "./TrendingCard";
+import { motion } from "framer-motion";
+
+const tabVariants = {
+  hidden: { opacity: 0, y: -20 },
+  visible: { opacity: 1, y: 0 },
+};
 
 const TrendingProducts = () => {
   const [allCategory] = useCategory();
@@ -11,16 +17,31 @@ const TrendingProducts = () => {
 
   return (
     <>
-      <div className=" w-11/12 mx-auto">
+      <div className="w-11/12 mx-auto">
         <SectionTitle
           title="Our Trending Organic & Fresh Products"
-          details="We Connect buyers & Sellers of Natural, Organic, Enviornmetnlly sound products."
+          details="We connect buyers & sellers of natural, organic, environmentally sound products."
         />
-        <section className=" mt-10">
+        <section className="mt-10">
           <Tabs>
-            <TabList>
+            <TabList className="flex overflow-x-auto whitespace-nowrap">
               {allCategory?.map((itm) => (
-                <Tab key={itm?._id}>{itm?.category_name}</Tab>
+                <Tab
+                  key={itm?._id}
+                  className="px-4 py-2 mr-4 rounded-md border border-[#EEAB0F] focus:outline-none hover:cursor-pointer"
+                >
+                  <motion.div
+                    variants={tabVariants}
+                    initial="hidden"
+                    animate="visible"
+                    transition={{
+                      duration: 0.5,
+                      delay: allCategory.indexOf(itm) * 0.1,
+                    }}
+                  >
+                    {itm?.category_name}
+                  </motion.div>
+                </Tab>
               ))}
             </TabList>
 
