67ea4c4adbd1caff7758e382f40f7475474de2f1
MacBook Pro <naym100m@gmail.com>
2024-06-27 18:31:22 +0600

    updated and install antd design and addde a modal

--- src/Pages/Cart/OrderModal.jsx
+++ src/Pages/Cart/OrderModal.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
+import { Modal } from "antd";
 
 const OrderModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <button
-        onClick={() => document.getElementById("my_modal_5").showModal()}
+        type="primary"
+        onClick={showModal}
         className="btn w-[100%] text-[#201700] bg-[#EEAB0F] hover:bg-[#EEAB0F] font-semibold flex items-center transition hover:scale-105 border-none rounded-none hover:rounded-md"
       >
         <MdAddShoppingCart className="text-2xl text-center" />
         ক্যাশ অন ডেলিভারিতে অর্ডার করুন
       </button>
-      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
-          <div className="modal-action">
-            <form method="dialog">
-              <button className="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </>
   );
 };
