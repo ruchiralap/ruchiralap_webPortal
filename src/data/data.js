dffb9d897d818892471e4597be9345d06b08b509
MacBook Pro <naym100m@gmail.com>
2024-07-06 17:07:37 +0600

    updated

--- src/data/data.js
+++ src/data/data.js
@@ -3,18 +3,18 @@ export const offer = [
     id: 1,
     title: "Get 10% off on Fruits Item",
     des: "Shop our selection of organic fresh vegetables in a discounted price. 10% off",
-    offer_img: "/src/assets/offerFiles/Fruits.png",
+    offer_img: "/offerFiles/Fruits.png",
   },
   {
     id: 2,
     title: "Get 15% Organic Vegetable",
     des: "Shop our selection of organic fresh vegetables in a discounted price. 10% off.",
-    offer_img: "/src/assets/offerFiles/Vegetable.png",
+    offer_img: "/offerFiles/Vegetable.png",
   },
   {
     id: 3,
     title: "Get 10% off on Special Item",
     des: "Shop our selection of organic fresh vegetables in a discounted price. 10% off.",
-    offer_img: "/src/assets/offerFiles/SpecialItem.png",
+    offer_img: "/offerFiles/SpecialItem.png",
   },
 ];
